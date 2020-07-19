9988ac279f95ec7408bdbe96fdcf043de1f3c8a6
Yuri Ramocan <yuri.ramocan@detroitlabs.com>
2020-07-19 15:04:14 -0400

    Initial commit

---
+++ DiceRoll/DiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollApp.swift
+//  DiceRoll
+//
+//  Created by Yuri Ramocan on 7/19/20.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
